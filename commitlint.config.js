adf5b44231f23f8d6b868435d3a8fda55580e3e2
SteveKingFord <332312568@qq.com>
2021-03-30 14:36:59 +0800

    格式化配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,18 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+/*
+ * @Author: kingford
+ * @Date: 2021-03-30 13:58:08
+ * @LastEditTime: 2021-03-30 14:36:31
+ */
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["upd", "feat", "fix", "refactor", "docs", "chore", "style", "revert"]],
+    "type-case": [0],
+    "type-empty": [0],
+    "scope-empty": [0],
+    "scope-case": [0],
+    "subject-full-stop": [0, "never"],
+    "subject-case": [0, "never"],
+    "header-max-length": [0, "always", 72],
+  },
+};
