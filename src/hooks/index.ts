dbeb205fbe1ca98a4451d2db4d9f3faece719ded
kingford <332312568@qq.com>
2021-04-03 15:03:53 +0800

    feat: 新增useArray

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: kingford
  * @Date: 2021-04-01 13:58:24
- * @LastEditTime: 2021-04-03 14:40:23
+ * @LastEditTime: 2021-04-03 15:00:16
  */
 import { useState, useEffect } from 'react';
 
@@ -12,11 +12,27 @@ export const useMount = (fn: () => void) => {
   }, []);
 };
 
-export const useDebounce = <V>(value: V, delay: number) => {
+export const useDebounce = <V>(value: V, delay?: number) => {
   const [debounceValue, setDebounceValue] = useState(value);
   useEffect(() => {
     const timeout = setTimeout(() => setDebounceValue(value), delay);
     return () => clearTimeout(timeout);
   }, [value, delay]);
   return debounceValue;
+};
+
+export const useArray = <T>(initialArray: T[]) => {
+  const [value, setValue] = useState(initialArray);
+  return {
+    value,
+    setValue,
+    add: (item: T) => {
+      setValue([...value, item]);
+    },
+    clear: () => setValue([]),
+    removeIndex: (index: number) => {
+      const copy = [...value];
+      copy.splice(index, 1);
+    },
+  };
 };
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,11 @@
 /*
  * @Author: kingford
  * @Date: 2021-04-01 13:50:14
- * @LastEditTime: 2021-04-03 14:39:07
+ * @LastEditTime: 2021-04-03 15:02:58
  */
-export const isFalsy = (value:unknown) => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
-export const cleanObject = (object:Object) => {
+export const cleanObject = (object: object) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
     // @ts-ignore
