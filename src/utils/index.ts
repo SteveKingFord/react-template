f2fbec9ccca0870f5155afc32d1e082749cff40f
SteveKingFord <332312568@qq.com>
2021-04-10 21:57:34 +0800

    feat: 删除无用参数

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,17 +1,16 @@
 /*
  * @Author: kingford
  * @Date: 2021-04-01 13:50:14
- * @LastEditTime: 2021-04-03 15:02:58
+ * @LastEditTime: 2021-04-10 21:56:31
  */
 export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
+export const isVoid = (value: unknown) => (value === undefined || value === '' || value === null ? false : !value);
 
-export const cleanObject = (object: object) => {
+export const cleanObject = (object: { [key: string]: unknown }) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
-    // @ts-ignore
     const value = result[key];
-    if (isFalsy(value)) {
-      // @ts-ignore
+    if (isVoid(value)) {
       delete result[key];
     }
   });
